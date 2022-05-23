3714de5f06e2f3d8173efadb819ce84a96e042d6
El Faisal Alaaeddine <105279781+El-FaisalAlaaeddine@users.noreply.github.com>
2022-05-23 10:33:58 +0300

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,25 @@
+const FirstPlayerRandom = Math.floor(Math.random() *6)+1;
+const imageFirstPlayer = 'assets/dice' + FirstPlayerRandom + '.png';
+document.getElementById('img1').setAttribute('src', imageFirstPlayer)
+
+
+const SecondPlayerRandom = Math.floor(Math.random() *6)+1;
+const imageSecondPlayer = 'assets/dice' + SecondPlayerRandom + '.png';
+document.getElementById('img2').setAttribute('src', imageSecondPlayer)
+
+
+if (FirstPlayerRandom > SecondPlayerRandom){
+    document.getElementById('id').innerHTML = "Player One wins!"
+}
+else if (FirstPlayerRandom < SecondPlayerRandom){
+    document.getElementById('id').innerHTML = "Player Two wins!"
+
+}
+else{
+    document.getElementById('id').innerHTML = "Draw!"
+
+}
+
+
+
+
